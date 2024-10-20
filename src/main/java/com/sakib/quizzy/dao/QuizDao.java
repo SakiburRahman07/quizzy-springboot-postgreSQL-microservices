abf641cbdf7d3b785be55529227a075558fae583
sakib <sakiburrahman11.msr@gmail.com>
2024-10-20 20:47:29 +0600

    delete

--- src/main/java/com/sakib/quizzy/dao/QuizDao.java
+++
@@ -1,7 +0,0 @@
-package com.sakib.quizzy.dao;
-
-import com.sakib.quizzy.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizDao extends JpaRepository<Quiz, Integer> {
-}
